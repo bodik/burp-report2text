70c20dbcdafe8e4a85674fbc90840ec68b588635
Radoslav Bodo <bodik@cesnet.cz>
2021-04-22 21:34:50 +0200

    extension: look'n'feel code as Java even if written in Python

--- report2text.py
+++ report2text.py
@@ -41,8 +41,7 @@ class ReportToTextMenuListener(ActionListener):
                 output.append('```\n%s\n```\n' % self.extension._helpers.bytesToString(request.getRequest()))
                 output.append('```\n%s\n```\n' % self.extension._helpers.bytesToString(request.getResponse()))
 
-        self.extension.setTabBackground(self.extension.COLOR_RED)
-        self.extension.tab_main_text.text = '\n'.join(output)
+        self.extension.setReportText('\n'.join(output))
 
 
 class BurpExtender(IBurpExtender, IContextMenuFactory, ITab, FocusListener):
@@ -52,60 +51,65 @@ class BurpExtender(IBurpExtender, IContextMenuFactory, ITab, FocusListener):
         """extension startup"""
 
         # commons
-        self.extension_name = 'Report2text'
+        self.EXTENSION_NAME = 'Report2text'
         self.COLOR_RED = Color(0xff6633)
         self.COLOR_BLACK = Color(0x0)
 
         self._callbacks = callbacks
         self._helpers = self._callbacks.getHelpers()
-        self._callbacks.setExtensionName(self.extension_name)
+        self._callbacks.setExtensionName(self.EXTENSION_NAME)
 
         # menu
         self._callbacks.registerContextMenuFactory(self)
 
         # output tab
-        self.tab_main_text = JTextArea('initial text')
-        self.tab_main_text.editable = False
-        self.tab_main_text.setLineWrap(True)
-        self.tab_main_text.setWrapStyleWord(True)
-        self.tab_main_text.addFocusListener(self)
+        self._mainTextArea = JTextArea('initial text')
+        self._mainTextArea.editable = False
+        self._mainTextArea.setLineWrap(True)
+        self._mainTextArea.setWrapStyleWord(True)
+        self._mainTextArea.addFocusListener(self)
 
-        self.tab = JPanel(BorderLayout())
-        self.tab.add(JScrollPane(self.tab_main_text))
+        self._tab = JPanel(BorderLayout())
+        self._tab.add(JScrollPane(self._mainTextArea))
         self._callbacks.addSuiteTab(self)
 
         return
 
     def createMenuItems(self, invocation):
-        """IContextMenuFactory; context menu handler"""
+        """iface IContextMenuFactory; context menu handler"""
 
-        menu = ArrayList()
+        menuItems = ArrayList()
         if invocation.getInvocationContext() == invocation.CONTEXT_SCANNER_RESULTS:
-            report_to_text_menu_item = JMenuItem('Report2text')
-            report_to_text_menu_item.addActionListener(ReportToTextMenuListener(self, invocation))
-            menu.add(report_to_text_menu_item)
-
-        return menu
+            menuItem = JMenuItem('Report2text')
+            menuItem.addActionListener(ReportToTextMenuListener(self, invocation))
+            menuItems.add(menuItem)
+        return menuItems
 
     def getTabCaption(self):
-        """ITab; Return the text to be displayed on the tab"""
+        """iface ITab; Return the text to be displayed on the tab"""
         
-        return 'Report2text'
+        return self.EXTENSION_NAME
                                 
     def getUiComponent(self):
-        """ITab; Passes the UI to burp"""
+        """iface ITab; Passes the UI to burp"""
 
-        return self.tab
-
-    def setTabBackground(self, color):
-        """set tab caption background"""
-
-        tabbed_pane = self.getUiComponent().getParent()
-        for idx in range(tabbed_pane.getTabCount()):
-            if tabbed_pane.getTitleAt(idx) == self.extension_name:
-                tabbed_pane.setBackgroundAt(idx, color);
+        return self._tab
 
     def focusGained(self, event):
-        """FocusListener; reset color on tab focus"""
+        """iface FocusListener; reset color on tab focus"""
 
-        self.setTabBackground(self.COLOR_BLACK)
+        self._setTabBackground(self.COLOR_BLACK)
+
+    def setReportText(self, text):
+        """set report text"""
+
+        self._setTabBackground(self.COLOR_RED)
+        self._mainTextArea.text = text
+
+    def _setTabBackground(self, color):
+        """set tab caption background"""
+
+        tabbedPane = self.getUiComponent().getParent()
+        for idx in range(tabbedPane.getTabCount()):
+            if tabbedPane.getTitleAt(idx) == self.EXTENSION_NAME:
+                tabbedPane.setBackgroundAt(idx, color);
