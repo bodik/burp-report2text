d086a2d27b5a2de4df50e19b3a44ae082a36b398
Radoslav Bodo <bodik@cesnet.cz>
2021-04-22 20:41:46 +0200

    general: bump version

--- report2text.py
+++ report2text.py
@@ -15,7 +15,7 @@ from java.util import ArrayList
 from javax.swing import JMenuItem, JPanel, JScrollPane, JTextArea
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 class ReportToTextMenuListener(ActionListener):
